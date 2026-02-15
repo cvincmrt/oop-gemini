cc1b76cc48c511052e8a7b4953541198bb729af9
cvincmrt <cvincmrt@gmail.com>
2026-02-15 20:15:34 +0100

    zacal som vytvarat class Database

---
+++ Class/Database.php
@@ -0,0 +1,11 @@
+<?php
+
+class Database{
+    private string $host = "localhost";
+    private string $db_name = "eshop";
+    private string $username = "root";
+    private string $password = "";
+    public $conn;
+
+
+}
--- index.php
+++ index.php
@@ -11,8 +11,10 @@ $kosik->pridajPolozku(new Produkt("dvere", 100));
 $kosik->pridajPolozku(new Elektronika("lampa", 250, 8));
 $kosik->pridajPolozku(new Potravina("paradajky", 50, "14. 02. 2026"));
 
-$platba = new Dobierka();
-$kosik->zaplatit($platba);
+//Visa, ApplePay, Dobierka
+$metoda = new Dobierka();
+
+$kosik->zaplatit($metoda);
 
 //$kosik->vypisKosik();
 //echo $kosik->celkovaSuma();
